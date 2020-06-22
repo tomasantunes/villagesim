7fcf9fab32a32df809fccd4377270a9c83a1b7df
Tomás Antunes <tomasantunes@gmail.com>
2020-06-22 16:38:17 +0100

    Update products

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import datetime
 import os
 
 def connect_db():
-	return sqlite3.connect("bookforms.db")
+	return sqlite3.connect("villagesim.db")
 
 db = connect_db()
 
@@ -21,7 +21,7 @@ def init():
 							name text,
                             price numeric,
                             daily_growth numeric,
-							qtty numeric,
+							qtty numeric
 						); """
 
 	c.execute(sql_products_table)
@@ -29,14 +29,34 @@ def init():
 	sql_users_table = """ CREATE TABLE IF NOT EXISTS users (
 							id integer PRIMARY KEY,
 							name text,
-							balance text,
+							balance text
 						); """
 
 	c.execute(sql_users_table)
 
+def getProducts():
+	db = connect_db()
+	c = db.execute('SELECT * FROM products')
+	rows = c.fetchall()
+	return rows
+
+@app.route("/add-product", methods=['POST'])
+def addProduct():
+	name = request.form.get('name', "")
+	price = request.form.get('price', "")
+
+	if (name != "" and price != ""):
+		db = connect_db()
+		db.execute('INSERT INTO products (name, price) VALUES (?, ?)', [name, price])
+		db.commit()
+		return redirect("/")
+	else:
+		return "Submission Invalid"
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    products = getProducts()
+    return render_template('home.html', products=products)
 
 if __name__ == "__main__":
     init()
