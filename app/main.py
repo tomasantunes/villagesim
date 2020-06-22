8b72c15f1eb606dc28536ecae3963697bd7b021c
Tomás Antunes <tomasantunes@gmail.com>
2020-06-22 16:03:56 +0100

    Update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,43 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+import sqlite3
+import datetime
+import os
+
+def connect_db():
+	return sqlite3.connect("bookforms.db")
+
+db = connect_db()
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.secret_key = "default_key"
+
+def init():
+	db = connect_db()
+	c = db.cursor()
+
+	sql_products_table = """ CREATE TABLE IF NOT EXISTS products (
+							id integer PRIMARY KEY,
+							name text,
+                            price numeric,
+                            daily_growth numeric,
+							qtty numeric,
+						); """
+
+	c.execute(sql_products_table)
+
+	sql_users_table = """ CREATE TABLE IF NOT EXISTS users (
+							id integer PRIMARY KEY,
+							name text,
+							balance text,
+						); """
+
+	c.execute(sql_users_table)
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
+    init()
     app.run(host='0.0.0.0', port=5000)
---
+++ docs/Census Form.md
@@ -0,0 +1,12 @@
+# Census Form
+
+- Name
+- Phone number
+- Address
+- Gender
+- Age
+- What's the headcount at your house?
+- Do you rent or own? Have a mortgage or own your home mortgage-free?
+- What are your qualifications?
+- What is your profession?
+- What is your yearly income?
